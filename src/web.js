44ab691a92eaf1add6b29d1472c1c34ccd4df620
TartarusMac <wangtianxin2000@msn.com>
2025-07-22 13:53:33 +0800

    增加对 Storage 的封装

--- src/web.js
+++ src/web.js
@@ -3,3 +3,20 @@ globalThis.newEle = (tag, ...classList) => {
 	classList.forEach(cls => ele.classList.add(cls));
 	return ele;
 };
+
+Storage.prototype.rawSetItem = Storage.prototype.setItem;
+Storage.prototype.rawGetItem = Storage.prototype.getItem;
+Storage.prototype.setItem = function (key, value) {
+	return this.rawSetItem(key, JSON.stringify(value));
+};
+Storage.prototype.getItem = function (key, defVal) {
+	let val = this.rawGetItem(key);
+	if (val === undefined || val === null) return defVal;
+	try {
+		val = JSON.parse(val);
+	}
+	catch {
+		return defVal;
+	}
+	return val;
+};
